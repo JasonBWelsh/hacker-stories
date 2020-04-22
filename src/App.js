be6c3c57fd4cd9b2877fc07a6a6e1a1bdbdb6a95
Jason Welsh <jasonbwelsh@gmail.com>
2020-04-22 15:43:04 -0400

    remove story item functionality added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import List from './components/List';
 import InputWithLabel from './components/InputWithLabel';
 import './App.css';
 
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = useState(localStorage.getItem(key) || initialState);
 
@@ -14,26 +33,8 @@ const useSemiPersistentState = (key, initialState) => {
 };
 
 function App() {
+  const [stories, setStories] = useState(initialStories);
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
-
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
 
   const searchedStories = stories.filter((story) =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
@@ -42,6 +43,13 @@ function App() {
   const handleSearch = (event) => {
     console.log('!!!', event.target.value);
     setSearchTerm(event.target.value);
+  };
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => story.objectID !== item.objectID
+    );
+    setStories(newStories);
   };
 
   return (
@@ -59,7 +67,7 @@ function App() {
 
       <hr />
 
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
-const List = ({ list }) =>
-  list.map((item) => <Item key={item.objectID} item={item} />);
+const List = ({ list, onRemoveItem }) =>
+  list.map((item) => (
+    <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+  ));
 
-const Item = ({ item }) => (
-  <div>
-    <span>
-      <a href={item.url}>{item.title}</a>
-    </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-  </div>
-);
+const Item = ({ item, onRemoveItem }) => {
+  const handleRemoveItem = () => onRemoveItem(item);
+
+  return (
+    <div>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      <button type="button" onClick={handleRemoveItem}>
+        x
+      </button>
+    </div>
+  );
+};
 
 export default List;
